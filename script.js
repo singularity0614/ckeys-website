79c7284a80df1b57a8ce04d55385c6e5a58ff80b
NoobAtManim <68496417+NoobAtManim@users.noreply.github.com>
2021-12-19 14:07:42 +1100

    searching ui

--- script.js
+++ script.js
@@ -9,9 +9,32 @@ exitButton.onclick = closeDiv;
 
 const searchButton = document.querySelector(".search");
 const iconList = document.querySelector(".icon-group");
+const leftIconList = document.querySelector(".left-icon-group");
 const rightIconList = document.querySelector(".right-icon-group");
+var isSearching = false;
+let intViewportWidth = window.innerWidth - 59.5;
 
 searchButton.onclick = function() {
-    iconList.classList.add("searchstart");
-    rightIconList.className += " hidden";
+    if (!isSearching) {
+        iconList.classList.add("searchstart");
+        var p = document.createElement("input");
+        p.type = "search";
+        p.placeholder = "Search..."
+        p.style.padding = "7px";
+        p.style.margin = "0 0 0 10px";
+        p.style.border = "0";
+        p.style.outline = "none";
+        p.style.fontFamily = "Bai Jamjuree";
+        p.style.fontSize = "16px";
+        p.style.left = "50px";
+        p.style.width = intViewportWidth + "px";
+        leftIconList.style.width = "50vw";
+        leftIconList.append(p);
+        rightIconList.remove();
+        p.focus();
+        isSearching = true;
+    } else {
+        p.remove();
+        isSearching = false;
+    }
 }
